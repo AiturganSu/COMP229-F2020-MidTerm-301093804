8d99dcb2bedc8a60adadbcbc726f169ae89bb451
Aiturgan Suiunbai kyzy <aiturgansuiunbaikyzy@Aiturgans-MacBook-Pro.local>
2020-10-27 00:57:58 +0600

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
-  //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+    //local MongoDB deployment ->
+    //"URI": "mongodb://localhost/books229"
+    "URI": "mongodb+srv://aiturgan:0V6OEFYakPxT3blw@mongodbserver.csibw.mongodb.net/book?retryWrites=true&w=majority"
 };
